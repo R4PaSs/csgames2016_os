a928d67e7f78353f1b876e469aafa3d2007dff83
Lucas Bajolet <r4pass@hotmail.com>
2015-10-21 21:32:31 -0400

    converters: Added metadata hierarchy building functions
    Signed-off-by: Lucas Bajolet <r4pass@hotmail.com>

--- converters/filecoder.c
+++ converters/filecoder.c
@@ -7,6 +7,7 @@
 #include "filecoder.h"
 #include <dirent.h>
 #include <string.h>
+#include <libgen.h>
 
 int main(int argc, char** argv)
 {
@@ -19,7 +20,8 @@ int main(int argc, char** argv)
 	struct stat s;
 	if(!stat(argv[1], &s)) {
 		if(S_ISDIR(s.st_mode) || S_ISREG(s.st_mode)) {
-			//make_filesystem(argv[1], out);
+			//printf("%d\n", in_byte_size(".", argv[1]));
+			make_filesystem(argv[1], out);
 		} else {
 			printf("Bad argument %s, expected path to a file or a directory\n", argv[1]);
 		}
@@ -28,6 +30,7 @@ int main(int argc, char** argv)
 	}
 }
 
+// Checks if option '-o' is used, defaults to "out"
 char* parse_out(int argc, char** argv)
 {
 	int i;
@@ -36,9 +39,10 @@ char* parse_out(int argc, char** argv)
 			return argv[i + 1];
 		}
 	}
-	return "out";
+	return "out.coal";
 }
 
+// Checks if option '-e' is used, defaults to 8
 int parse_ext(int argc, char** argv)
 {
 	int i = 0;
@@ -50,17 +54,98 @@ int parse_ext(int argc, char** argv)
 	return 8;
 }
 
-void make_filesystem(char* root_path, char* out_path, filepart fp)
+// Makes the filesystem from the files or directories a
+// `root_path` and outputs the binary data to `out_path`
+void make_filesystem(char* root_path, char* out_path)
 {
 	int byte_size = in_byte_size(".", root_path);
 	FILE* out = fopen(out_path, "w");
-	build_header(out, fp);
+	dirmeta* top = build_hierarchy(root_path);
+	write_fs_to(top, out_path);
 	struct stat s;
 }
 
-void build_header(FILE* out, filepart fp)
-{
+// Builds an in-memory file-hierarchy with a few metadata
+dirmeta* build_hierarchy(char* top_path) {
+	dirmeta* top = calloc(1, sizeof(dirmeta));
+	top->dir_id = 0;
+	struct stat s;
+	int next_id = 1;
+	if(!stat(top_path, &s)) {
+		if(S_ISDIR(s.st_mode)) {
+			next_id = add_folder_to(top_path, top, next_id);
+		} else if (S_ISREG(s.st_mode)) {
+			add_file_to(top_path, top);
+		}
+	}
 }
+
+// Adds folder at `path` to the hierarchy of `d` with id `next`
+int add_folder_to(char* path, dirmeta* d, int next) {
+	dirmeta* curr = calloc(1, sizeof(dirmeta));
+	curr->parent_dir = d->dir_id;
+	curr->dir_id = next;
+	curr->src_path = path;
+	metalist* m = calloc(1, sizeof(metalist));
+	m->magic = 0x80;
+	m->metadata = curr;
+	if(d->children != NULL) {
+		append_meta_to_list(d->children, m);
+	}else{
+		d->children = m;
+	}
+	char* bn = basename(path);
+	int nlen = strlen(bn);
+	if(nlen >= 50) nlen = 49;
+	memmove(curr->dir_name, bn, nlen);
+	next += 1;
+	DIR* dir = opendir(path);
+	struct dirent* dent = readdir(dir);
+	struct stat st;
+	while(dent != NULL) {
+		char* bn = dent->d_name;
+		char* strs[] = {path, bn};
+		char* s = join(strs, 2, "/");
+		if(strcmp(bn, ".") && strcmp(bn, "..") && !stat(s, &st)) {
+			if(S_ISDIR(st.st_mode)) {
+				next = add_folder_to(s, curr, next);
+			} else if(S_ISREG(st.st_mode)) {
+				add_file_to(s, curr);
+			}
+		}
+		dent = readdir(dir);
+		free(s);
+	}
+	closedir(dir);
+	return next;
+}
+
+// Adds file at `path` to the hierarchy of `d`
+void add_file_to(char* path, dirmeta* d) {
+	filemeta* fm = calloc(1, sizeof(filemeta));
+	fm->parent_dir = d->dir_id;
+	fm->byte_size = in_byte_size("", path);
+	fm->src_path = path;
+	char* bn = basename(path);
+	int bnln = strlen(bn);
+	metalist* m = calloc(1, sizeof(metalist));
+	m->magic = 0x40;
+	m->metadata = fm;
+	if(d->children != NULL) {
+		append_meta_to_list(d->children, m);
+	}else{
+		d->children = m;
+	}
+}
+
+void append_meta_to_list(metalist* lst, metalist* node) {
+	metalist* lnk = lst;
+	while(lnk->next != NULL) {
+		lnk = lnk->next;
+	}
+	lnk->next = node;
+}
+
 
 int in_byte_size(char* parent_path, char*inpath)
 {
@@ -78,11 +163,14 @@ int in_byte_size(char* parent_path, char*inpath)
 
 int in_file_byte_size(char* parent_path, char* in_path)
 {
-	int f = open(in_path, O_RDONLY);
+	char* ss[] = {parent_path, in_path};
+	char* path = join(ss, 2, "/");
+	int f = open(path, O_RDONLY);
 	struct stat s;
 	if(!fstat(f, &s)) {
 		return s.st_size;
 	}
+	free(path);
 	return 0;
 }
 
@@ -98,19 +186,9 @@ int in_dir_byte_size(char* parent_path, char* in_path)
 	struct stat s;
 	while(dent != NULL) {
 		char* file = dent->d_name;
-		int path_sz = 0;
-		path_sz += strlen(parent_path);
-		path_sz++;
-		path_sz += strlen(in_path);
-		path_sz++;
-		path_sz += strlen(file);
-		char* path = calloc(path_sz, 1);
-		strcat(path, parent_path);
-		strcat(path, "/");
-		strcat(path, in_path);
-		strcat(path, "/");
-		strcat(path, file);
-		if(strcmp(file, ".") && strcmp(file, "..") && !stat(file, &s)){
+		char* strs[] = {parent_path, in_path, file};
+		char* path = join(strs, 3, "/");
+		if(strcmp(file, ".") && strcmp(file, "..") && !stat(path, &s)){
 			if(S_ISDIR(s.st_mode) || S_ISREG(s.st_mode)){
 				size += in_byte_size(parent_path, path);
 			}
@@ -122,13 +200,21 @@ int in_dir_byte_size(char* parent_path, char* in_path)
 	return size;
 }
 
-void encode_dir(DIR* d, FILE* out)
+// Joins an array of strings using `sep`
+char* join(char** strings, int len, char* sep)
 {
-
-}
-
-// Encode a file `f` to `out`
-void encode_file(FILE* f, FILE* out)
-{
-
+	int i;
+	if(len == 0) return "";
+	int endlen = 0;
+	for(i = 0; i < len; i++) {
+		endlen += strlen(strings[i]);
+	}
+	endlen += strlen(sep) * (len - 1);
+	char* endstr = calloc(endlen + 1, 1);
+	strcat(endstr, strings[0]);
+	for(i = 1; i < len; i++) {
+		strcat(endstr, sep);
+		strcat(endstr, strings[i]);
+	}
+	return endstr;
 }
--- converters/filecoder.h
+++ converters/filecoder.h
@@ -2,8 +2,6 @@
 
 // File metadata
 typedef struct filemeta {
-	// Must be 0x40 for a file
-	char file_magic;
 	// Parent directory id, unsigned 32 bits
 	uint32_t parent_dir;
 	// File name in UTF-8
@@ -14,12 +12,20 @@ typedef struct filemeta {
 	uint32_t ext_size;
 	// File size in bytes
 	uint32_t byte_size;
+	// Path to source file
+	char* src_path;
 } filemeta;
 
 // A data block for a file
 typedef struct filedata {
-	// Location in extents of the file data
-	uint32_t location;
+	// First byte determines if the file data is a continuation block or not
+	uint8_t continuation_header;
+	// Size of the block in bytes
+	uint32_t byte_size;
+	// Data of the file, size up to
+	char* data;
+	// Next block's location (in extents)
+	uint32_t continuation_location;
 } filedata;
 
 // A File partition
@@ -44,12 +50,44 @@ typedef struct filepart {
 	char* mask;
 } filepart;
 
+typedef struct metalist metalist;
+
+typedef struct metalist {
+	// Magic number, 0x80 is a dirmeta, 0x40 is a filemeta
+	uint8_t magic;
+	// Pointer to metadata, if magic is 0x80 then cast to dirmeta, else cast to filemeta
+	void* metadata;
+	// Pointer to the next entity
+	metalist* next;
+} metalist;
+
+// Directory metadata
+typedef struct dirmeta {
+	// Identifier of directory in memory
+	uint32_t dir_id;
+	// Parent directory's id
+	uint32_t parent_dir;
+	// Name of the directory, must be null-terminated
+	char dir_name[50];
+	// Location of the data in extents
+	uint32_t data_location;
+	// Children data
+	metalist* children;
+	// Path to source directory
+	char* src_path;
+} dirmeta;
+
 char* parse_out(int argc, char** argv);
 int parse_ext(int argc, char** argv);
-void make_filesystem(char* root_path, char* out_path, filepart fp);
-void build_header(FILE* out, filepart fp);
-void encode_file(FILE* f, FILE* out);
-void encode_dir(DIR* d, FILE* out);
+void make_filesystem(char* root_path, char* out_path);
 int in_byte_size(char* parent_path, char* inpath);
 int in_file_byte_size(char* parent_path, char* in);
 int in_dir_byte_size(char* parent_path, char* in_path);
+char* join(char** strings, int len, char* sep);
+dirmeta* build_hierarchy(char* top_path);
+void add_file_to(char* path, dirmeta* d);
+int add_folder_to(char* path, dirmeta* d, int next);
+uint32_t file_byte_to_extent_size(long byte_size);
+void append_meta_to_list(metalist* lst, metalist* node);
+filedata* encode_file(filepart* fp, char* f);
+void write_fs_to(dirmeta* d, char* out_path);
